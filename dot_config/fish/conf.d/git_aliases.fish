4e7c6a9b29c090448860469425b469dd37384fa3
Jonas Lundholm Bertelsen <drixi.b@gmail.com>
2025-11-01 21:08:59 +0100

    Initial commit

---
+++ dot_config/fish/conf.d/git_aliases.fish
@@ -0,0 +1,18 @@
+function gabbr --argument-names alias cmd
+    abbr -a $alias git $cmd
+end
+
+gabbr gco checkout
+gabbr gc commit
+gabbr gst status
+gabbr gl log
+gabbr gps push
+gabbr gpl pull
+gabbr ga add
+gabbr gd diff
+gabbr gbr branch
+gabbr grb rebase
+gabbr grs reset
+gabbr gmg merge
+functions -e gabbr
+
