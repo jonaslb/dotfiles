5b0d72fe7b603604073a0ceafe127a6ea1e15c90
Jonas Lundholm Bertelsen <drixi.b@gmail.com>
2025-11-29 22:11:43 +0100

    Add rust paths

--- dot_config/fish/conf.d/paths.fish
+++ dot_config/fish/conf.d/paths.fish
@@ -1,2 +1,5 @@
 set -U fish_user_paths $fish_user_paths $HOME/.local/bin
+set -Ux CARGO_HOME $HOME/.local/cargo
+set -Ux RUSTUP_HOME $HOME/.local/rustup
+set -U fish_user_paths $fish_user_paths $CARGO_HOME/bin
 
